ccc812f2b98a78f25c051cce8975d2d19f6ad942
Nicholas Ho <hjhnicholas@yahoo.com.sg>
2022-05-15 20:51:44 +0800

    Hang man game

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React,{ useState, useEffect} from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/Wrongletters';
+import Word from './components/Word';
+import Notification from './components/Notification';
+import Popup from './components/Popup';
+import {showNotification as show} from './helpers/helpers';
+
+import './App.css';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setcorrectLetters] = useState([]);
+  const [wrongLetters, setwrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(()=>{
+    const handleKeydown = event => {
+      const {key, keyCode} = event;
+        if (playable && keyCode >= 65 && keyCode <= 90) {
+          const letter = key.toLowerCase();
+    
+          if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+              setcorrectLetters(currentLetters => [...currentLetters, letter])
+            } else {
+              show(setShowNotification);
+            }
+          } else {
+            if (!wrongLetters.includes(letter)) {
+              setwrongLetters(wrongLetters => [...wrongLetters, letter])
+            } else {
+              show(setShowNotification);
+            }
+          }
+        }
+      }
+  window.addEventListener('keydown', handleKeydown);
+
+    return () => window.removeEventListener('keydown', handleKeydown);
+  },[correctLetters, wrongLetters, playable]);
+
+  function playAgain(){
+    setPlayable(true);
+    
+    //Empty Arrays
+    setcorrectLetters([]);
+    setwrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
+  return (
+    <>
+      <Header />
+      <div className='game-container'>
+        <Figure wrongLetters={wrongLetters}/>
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
+      </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} 
+      selectedWord ={selectedWord} setPlayable={setPlayable} playAgain={playAgain}/>
+      <Notification showNotification={showNotification}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <>
+    <h1>Hangman</h1>
+    <p>Find the hindden word - Enter a Letter</p>
+    </>
+  )
+}
+
+export default header
