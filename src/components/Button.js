98a5762f519f35b4b79cf83aef0612de6bb31ae6
Nicholas Ho <hjhnicholas@yahoo.com.sg>
2022-05-22 16:32:06 +0800

    Centralise texts to be compatible with phone

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,32 +4,41 @@ const Button = ({a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}) => {
   return (
 
     <div className="button-container">
-    <button onClick={a}>A</button>
-    <button onClick={b}>B</button>
-    <button onClick={c}>C</button>
-    <button onClick={d}>D</button>
-    <button onClick={e}>E</button>
-    <button onClick={f}>F</button>
-    <button onClick={g}>G</button>
-    <button onClick={h}>H</button>
-    <button onClick={i}>I</button>
-    <button onClick={j}>J</button>
-    <button onClick={k}>K</button>
-    <button onClick={l}>L</button>
-    <button onClick={m}>M</button>
-    <button onClick={n}>N</button>
-    <button onClick={o}>O</button>
-    <button onClick={p}>P</button>
-    <button onClick={q}>Q</button>
-    <button onClick={r}>R</button>
-    <button onClick={s}>S</button>
-    <button onClick={t}>T</button>
-    <button onClick={u}>U</button>
-    <button onClick={v}>V</button>
-    <button onClick={w}>W</button>
-    <button onClick={x}>X</button>
-    <button onClick={y}>Y</button>
+
+    <h1>  
+      <button onClick={q}>Q</button>
+      <button onClick={w}>W</button>
+      <button onClick={e}>E</button>
+      <button onClick={r}>R</button>
+      <button onClick={t}>T</button>
+      <button onClick={y}>Y</button>
+      <button onClick={u}>U</button>
+      <button onClick={i}>I</button>
+      <button onClick={o}>O</button>
+      <button onClick={p}>P</button>
+      </h1>
+
+    <h2>
+      <button onClick={a}>A</button> 
+      <button onClick={s}>S</button> 
+      <button onClick={d}>D</button>
+      <button onClick={f}>F</button>
+      <button onClick={g}>G</button>
+      <button onClick={h}>H</button>
+      <button onClick={j}>J</button>
+      <button onClick={k}>K</button>
+      <button onClick={l}>L</button>
+      </h2>  
+  
+    <h3>
     <button onClick={z}>Z</button>
+    <button onClick={x}>X</button>
+    <button onClick={c}>C</button>
+    <button onClick={v}>V</button>
+    <button onClick={b}>B</button>
+    <button onClick={n}>N</button>
+    <button onClick={m}>M</button>
+    </h3> 
 
   </div>
   )
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Notification = ({showNotification}) => {
   return (
     <div className={`notification-container${showNotification ? 'show' : ''}`}>
-      <p>You have already entered this letter</p>
+      <h2>You have already entered this letter</h2>
     </div>
   )
 }
