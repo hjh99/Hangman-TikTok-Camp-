254aec69f34c162e57e7574ac6b46c0c529791d1
amreeeta <77422453+amreeeta@users.noreply.github.com>
2022-05-25 18:40:29 +0800

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,9 @@ import React from 'react'
 const header = () => {
   return (
     <>
-    <h1>Hangman</h1>
-    <p>100 of the hardest hangman words - Enter a Letter</p>
+    <h1>Welcome to Hangman!</h1>
+    <p>This game uses the top 100 hardest Hangman words so good luck!- You'll need it. </p>
+    <p>Start the game by choosing a letter from the options below. You can make up to 6 mistakes so choose wisely.</p>
     </>
   )
 }
